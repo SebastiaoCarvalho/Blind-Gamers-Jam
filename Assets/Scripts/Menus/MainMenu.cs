f08cb3272fb505d95e5246e429625546d6ad75f7
Sebastiao Carvalho <sebastiaovscarvalho@tecnico.ulisboa.pt>
2024-02-29 15:16:41 +0000

    add more menu logic

--- Assets/Scripts/Menus/MainMenu.cs
+++ Assets/Scripts/Menus/MainMenu.cs
@@ -1,13 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+    public Button[] buttons;
+    private int selectedButton = 0;
     // Start is called before the first frame update
     void Start()
     {
-        
+        buttons[selectedButton].Select();
     }
 
     // Update is called once per frame
@@ -21,10 +24,15 @@ public class MainMenu : MonoBehaviour
     }
 
     public void OptionsButton() {
-        /* UnityEngine.SceneManagement.SceneManager.LoadScene("OptionsMenu"); */
+        UnityEngine.SceneManagement.SceneManager.LoadScene("OptionsMenu");
     }
 
     public void QuitButton() {
         Application.Quit();
+    }
+
+    public void Up() {
+        selectedButton = (selectedButton + buttons.Length) % buttons.Length;
+        buttons[selectedButton].Select();
     }
 }
---
+++ Assets/Scripts/Menus/OptionsMenu.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OptionsMenu : MonoBehaviour
+{
+    public GameObject soundMenuUI;
+    public GameObject controlsMenuUI;
+    public Slider[] sliders;
+    
+    void Start() {
+        foreach (Slider slider in sliders) {
+            slider.onValueChanged.AddListener(delegate {SliderChange(slider);});
+        }
+    }
+
+    public void SoundButton() {
+        soundMenuUI.SetActive(true);
+        controlsMenuUI.SetActive(false);
+    }
+
+    public void ControlsButton() {
+        soundMenuUI.SetActive(false);
+        controlsMenuUI.SetActive(true);
+    }
+
+    public void SliderChange(Slider slider) {
+        Debug.Log(slider);
+        Debug.Log(slider.gameObject.transform.parent.GetChild(2));
+        slider.gameObject.transform.parent.GetChild(2).GetComponent<TextMeshProUGUI>().text 
+            = GetSliderValuePretty(slider.value); // update the text of the slider
+    }
+
+    private string GetSliderValuePretty(float value) {
+        return (Math.Round(value, 2) * 100).ToString();
+    }
+
+}
